f9d9469802328a33820dbac30616ae4ef8a83f94
Unknown <kikozidan@gmail.com>
2018-10-29 16:09:34 +0100

    cleaning and enhancing the code, add some notices to the report

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -3,22 +3,38 @@ import random as rand
 import math
 from matplotlib import pyplot
 
-N = 100
-c = 6
-Lambda = 5   # for regularization 
-polynom_order = 20
-check_case = 2
+def generate_data_set(N):
+    """
+        this function is to generate the points (x,y)
+        N: number of points to generate
+    """
+    x_values = []
+    epsilon_vec = create_epsilon_vector(0,0.1,N)
+    for i in range(0, N):
+        x_values.append(i/(N/10))  
+    y_values = []
+    for i in range(0, N):        
+        y_values.append(quadratic_func(i, epsilon_vec[rand.randint(0,N*N-1)]))
+        #print("Range:", i , " | Quadradit Value: " , val)
+    plot_points(x_values,y_values)
+    return x_values,y_values
 
-def quadratic_func(x, epsilon):
-    return x**2 + x + c + epsilon
-
-def create_epsilon_vector(variance,mean):
+def create_epsilon_vector(variance=0,mean=0.1,N):
+    """
+    using a constant variance = 0 and a mean = 0.1 giving a good normal distributed points
+    """
     epsilon_vec = np.random.normal(mean,variance,N*N)
     count, bins, ignored = pyplot.hist(epsilon_vec, 30, density=True)
     pyplot.plot(bins, 1/(variance * np.sqrt(2 * np.pi)) *np.exp( - (bins - mean)**2 / (2 * variance**2) ),linewidth=2, color='r')
     pyplot.show()
     #print(epsilon_vec)
     return epsilon_vec
+
+def quadratic_func(x, epsilon):
+    """
+        non-linear quadratic function to generate the points
+    """
+    return x**2 + x + 6 + epsilon
 
 def compute_epsilon(x_values):
     if len(x_values) == 0:
@@ -32,20 +48,6 @@ def compute_epsilon(x_values):
     for x in x_values:
         normalized_sum += (x-mean)**2
     return (normalized_sum/len(x_values)),mean    
-
-def generate_data_set():
-    x_values = []
-    epsilon_vec = create_epsilon_vector(0,0.1)
-    for i in range(0, N):
-        x_values.append(i/(N/10))
-    #epsilon,_ = compute_epsilon(x_values)    
-    y_values = []
-    for i in range(0, N):        
-        y_values.append(quadratic_func(i, epsilon_vec[rand.randint(0,N*N-1)]))
-        #print("Range:", i , " | Quadradit Value: " , val)
-    plot_points(x_values,y_values)
-    return x_values,y_values
-
 
 def sigma(order, x_values):
     result = 0
@@ -86,24 +88,11 @@ def generate_general_A_matrix(m_order, x_values):
         A.append(tmp)
     return A
 
-def generate_initial_weights_vector(polynom_order,A,b):
-    return np.matmul(A,b)
-
 def generate_initial_weights_randomly(polynom_order):
     weights_vec = []
     for i in range(0,polynom_order+1):
         weights_vec.append(rand.random())
     return weights_vec
-    
-
-def generate_b_vec(x_values,y_values,polynom_order):
-    B = []
-    for i in range(0,polynom_order+1):
-        sum_y = 0
-        for index,y in enumerate(y_values):
-            sum_y += y + x_values[index]**i
-        B.append(sum_y )
-    return np.array(B)
 
 def compute_new_weights(phi, predicted_values,Lambda):
     before_lambda = np.matmul(np.transpose(phi),phi)
@@ -139,7 +128,7 @@ def root_mean_square_error(predicted_values, y_values, weights, mLambda):
     erms = math.sqrt((sum_error+abs(lambda_term) )/len(predicted_values))
     return erms
     
-def erms_plot_k_folds(x_values, y_values, k, lambda_max):
+def erms_plot_k_folds(x_values, y_values, k, lambda_max, polynom_order,new_Y):
     erms_list = []
     erms_list_trainings_set = []
 
@@ -202,7 +191,7 @@ def erms_plot_k_folds(x_values, y_values, k, lambda_max):
     lambda_set = [i for i in range(0,lambda_max)]
     plot_points_two_set(lambda_set, erms_list_trainings_set, lambda_set, erms_list)
         
-def plot_with_and_without_regularization(x_values, y_values, lambda_list):
+def plot_with_and_without_regularization(x_values, y_values, lambda_list,polynom_order):
     phi = np.array(computed_phi(x_values, polynom_order) )
     result = []
     for mLambda in lambda_list:
@@ -223,37 +212,44 @@ def plot_with_and_without_regularization(x_values, y_values, lambda_list):
                    'lambda:'+str(lambda_list[3])))
     pyplot.show()        
 
+def linear_regrssion_model(NUM_Points = 100, Lambda = 5 , polynom_order = 8):
+    """
+        main function to perform the linear regression model.
+        
+        NUM_Points: number of generated points
+        Lambda: lambda value  for regularization 
+        polynom_order: the M-th polynomial order value
+    """
 
-x_values,y_values = generate_data_set()
-#not used
-#variance, mean = compute_epsilon(x_values)
-#epsilon_vec = create_epsilon_vector(variance, mean)
-matrixA = np.array(generate_general_A_matrix(polynom_order, x_values) )
-
-if check_case == 1:
-    #case1
-    vec_B = generate_b_vec(x_values,y_values,polynom_order)
-    weightVec = generate_initial_weights_vector(polynom_order,matrixA,vec_B)
-else:    
-    #case2
+    # TODO: generate data-sets of (x,y) points
+    x_values,y_values = generate_data_set(NUM_Points)
+    
+    # generate the known matrices for A.*W = b 
+    matrix_A = np.array(generate_general_A_matrix(polynom_order, x_values) )
     weightVec = generate_initial_weights_randomly(polynom_order)
-    vec_B = compute_b_matrix(matrixA, weightVec)
+    vec_B = compute_b_matrix(matrix_A, weightVec)
 
-phi = np.array(computed_phi(x_values, polynom_order) )
-Vec_T = predict_t_value(phi, weightVec)
-newWeightVec = compute_new_weights(phi, y_values,Lambda)
+    # Design Matrix PHI
+    phi = np.array(computed_phi(x_values, polynom_order) )
+    
+    # calculate the new Weights using the pseudo-inverse 
+    newWeightVec = compute_new_weights(phi, y_values,Lambda)
+    print(newWeightVec)
+    
+    # calculate the new generated Y_values for validating solution
+    new_Y = np.matmul( phi,newWeightVec )
+    
+    plot_points(x_values,y_values)       
+    plot_points(x_values,new_Y,'red')
+    
+    erms = root_mean_square_error(new_Y, y_values, newWeightVec, Lambda)      
+    print("Root-Mean-Square-Error")
+    print(erms)
+    
+    erms_plot_k_folds(x_values, y_values, 4, 10,polynom_order,new_Y)
+    
+    plot_with_and_without_regularization(x_values, y_values, [0,5,-18,-64],polynom_order)
 
-new_Vec_T = predict_t_value(phi, newWeightVec)
-B_temp = np.matmul((np.transpose(phi)),new_Vec_T)
-new_Y = np.matmul( phi,newWeightVec )
 
-plot_points(x_values,y_values)       
-plot_points(x_values,new_Y,'red')
-
-erms = root_mean_square_error(new_Y, y_values, newWeightVec, Lambda)      
-print("Root-Mean-Square-Error")
-print(erms)
-
-erms_plot_k_folds(x_values, y_values, 4, 10)
-
-plot_with_and_without_regularization(x_values, y_values, [0,5,-18,-64])
+if __name__ == "__main__":
+    linear_regrssion_model()
