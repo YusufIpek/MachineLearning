becb2d1d9ff8518a98a4a0cd98edef9bad52912b
kamalelsaaid <kamal.zakieldin@arete-global.com>
2019-01-06 09:16:25 +0200

    set the environment, currently working gym library

--- Assignment 3/gym_car.py
+++ Assignment 3/gym_car.py
@@ -2,3 +2,10 @@ import gym
 # Create the gym environment
 env = gym.make("MountainCar-v0")
 env.seed(3333) # Set a seed for reproducability
+
+#env = gym.make('CartPole-v0')
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) # take a random action
+
