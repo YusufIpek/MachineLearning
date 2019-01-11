55552a1f5576524bcd506572df02b724e85f3ac5
kamalelsaaid <kamal.zakieldin@arete-global.com>
2019-01-11 11:19:16 +0100

    not working example

--- Assignment 3/q-learning_algo.py
+++ Assignment 3/q-learning_algo.py
@@ -2,17 +2,17 @@ import numpy as np
 import gym
 #from gym import wrappers
 
-n_states = 40
-max_episodes = 10000
-initial_lr = 1.0 #Initial Learning rate
-min_lr = 0.003
+n_states = 40 #TODO: check the best num of states to use ( tune )
+max_episodes = 20000
+initial_lr = 0.9 #Initial Learning rate
+min_lr = 0.01 # lowest learning rate
 discount_factor = 0.9
 max_iterations = 10000
-eps = 0.02
+epsilon = 0.6  # act non-greedy or state-action have no value #TODO: tune to higher values
 env_name = 'MountainCar-v0'
 env = gym.make(env_name)
-env.seed(0)
-np.random.seed(0)
+env.seed(3333)
+np.random.seed(3333)
 q_table = np.zeros((n_states, n_states, env.action_space.n))
 
 def train(render=False):
@@ -25,7 +25,8 @@ def train(render=False):
             if render:
                 env.render()
             state = obs_to_state(obs)
-            if np.random.uniform(0, 1) < eps:
+
+            if np.random.uniform(0, 1) < epsilon: # act non-greedy or state-action have no value
                 action = np.random.choice(env.action_space.n)
             else:
                 logits = q_table[state]
@@ -38,6 +39,8 @@ def train(render=False):
             new_state = obs_to_state(obs)
             q_table[state + (action,)] = q_table[state + (action,)] + eta * (reward + discount_factor *  np.max(q_table[new_state]) - q_table[state + (action, )])
             if done:
+                if iter + 1 != 200 :
+                    print("Episode finished after {} timesteps".format(iter + 1))
                 break
         if ep % 500 == 0:
             print('Iteration #{} -- Total reward = {}.'.format(ep+1, total_reward))
@@ -52,12 +55,15 @@ def run(render=True, policy=None):
         if policy is None:
             action = env.action_space.sample()
         else:
+            # just applying the learned policy
             state =  obs_to_state(obs)
             action = policy[state]
         obs, reward, done, info = env.step(action)
         total_reward += discount_factor ** step_idx * reward
         step_idx += 1
         if done:
+            if iter + 1 != 200:
+                print("Episode finished after {} timesteps".format(iter + 1))
             break
     return total_reward
 
@@ -71,12 +77,15 @@ def obs_to_state(obs):
     return a, b
 
 if __name__ == '__main__':
-    train(render=True)
+    train(render=False)
     solution_policy = np.argmax(q_table, axis=2)
     print("Solution policy")
     print(q_table)
+    #TODO: Save q-table
 
     # Animate it
     solution_policy_scores = [run(render=False, policy=solution_policy) for _ in range(100)]
     print("Average score of solution = ", np.mean(solution_policy_scores))
-    run(render=True, policy=solution_policy)
+    for _ in range(5):
+        # in case of solving the problem, to just provide the solution directly
+        run(render=True, policy=solution_policy)
